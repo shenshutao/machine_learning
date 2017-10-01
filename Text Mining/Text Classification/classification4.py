0362440ebf1491af91f970310f51518588839b28
SHUTAO SHEN <sstwood@gmail.com>
2017-10-01 15:46:00 +0800

    modify a bit

--- Text Mining/Text Classification/classification4.py
+++ Text Mining/Text Classification/classification4.py
@@ -7,7 +7,7 @@ news.head()
 news.groupby('Class').describe()
 
 #Select a subset from the dataframe. (crude money-fx trade)
-subnews=news[(news.Class=="trade")| (news.Class=='crude')|(news.Class=='money-fx') ]
+subnews=news[(news.Class=="trade")|(news.Class=='crude')|(news.Class=='money-fx')]
 subnews.groupby('Class').describe()
 print(subnews.shape)
 
@@ -19,14 +19,12 @@ subnews.head()
 #Plot the distribution of the document length for each category
 import matplotlib.pyplot as plt
 subnews.hist(column='Length',by='Class',bins=50)
-
 plt.show()
-subnews.head()
 
 #Define preprocessing function
 import nltk
 from nltk.corpus import stopwords
-newstopwords=stopwords.words("English") + ['yuhao','the','is','it','may']
+newstopwords=stopwords.words("English") + ['yuhao','the','is','it','may','shutao'] # add some stopwords by domain knowledge
 WNlemma = nltk.WordNetLemmatizer()
 
 def pre_process(text):
@@ -38,62 +36,41 @@ def pre_process(text):
 
 #Apply the function on each document
 subnews['Text'] = subnews['Text'].apply(pre_process)
-subnews.head()
+subnews['Text'].head()
 
-length=subnews['Text'].apply(len)
-subnews=subnews.assign(Length=length)
-subnews.head()
-
-#split the data into training and testing
+#split the data into training and testing, 7 : 3
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(subnews.Text, subnews.Class, test_size=0.33, random_state=12)
+X_train, X_test, y_train, y_test = train_test_split(subnews.Text, subnews.Class, test_size=0.3, random_state=12)
 
-#Create dtm by using word occurence
+#Create DTM (document-term matrix) by using word occurence
 from sklearn.feature_extraction.text import CountVectorizer
-
-
 count_vect = CountVectorizer( )
-
 X_train_counts = count_vect.fit_transform(X_train)
 X_train_counts.shape
+count_vect.get_feature_names()[:10]
 
-count_vect.get_feature_names()
-
-
-dtm1 = pd.DataFrame(X_train_counts.toarray().transpose(), index = count_vect.get_feature_names())
-dtm1=dtm1.transpose()
-dtm1.head()
-dtm1.to_csv('dtm1.csv',sep=',')
-
-
-# # Building a Naïve Bayes Model
+#dtm1 = pd.DataFrame(X_train_counts.toarray().transpose(), index = count_vect.get_feature_names())
+#dtm1=dtm1.transpose()
+#dtm1.head()
+#dtm1.to_csv('dtm1.csv',sep=',')
 
 #Create dtm by using Term Frequency. 
-#Divide the number of occurrences of each word in a document 
-#by the total number of words in the document: 
-#these new features are called tf for Term Frequencies
-#If set use_idf=True, which mean create dtm by using tf_idf
-
+#Set use_idf=False means use tf table only, not tf-idf.
 from sklearn.feature_extraction.text import TfidfTransformer
 tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
 X_train_tf = tf_transformer.transform(X_train_counts)
 X_train_tf.shape
 
+# # Building Modeling by using Naïve Bayes
+#from sklearn.naive_bayes import MultinomialNB
+#clf = MultinomialNB().fit(X_train_tf, y_train)
 
-#Building Modeling by using Naïve Bayes
-from sklearn.naive_bayes import MultinomialNB
-clf = MultinomialNB().fit(X_train_tf, y_train)
-
-
-#Prediction on new documents
-docs_new = ['Crude price is dropping ', 'rate is increasing']
-
-X_new_counts = count_vect.transform(docs_new)
-X_new_tfidf = tf_transformer.transform(X_new_counts)
-#X_new_tfidf = tfidf_transformer.transform(X_new_counts)
-
-predicted = clf.predict(X_new_tfidf)
-print(predicted)
+##Prediction on new documents
+#docs_new = ['Crude price is dropping ', 'rate is increasing']
+#X_new_counts = count_vect.transform(docs_new)
+#X_new_tfidf = tf_transformer.transform(X_new_counts)
+#predicted = clf.predict(X_new_tfidf)
+#print(predicted)
 
 #Build a pipeline: Combine multiple steps into one
 from sklearn.pipeline import Pipeline
@@ -102,21 +79,20 @@ text_clf = Pipeline([('vect', CountVectorizer()),
                       ('clf', MultinomialNB()),
                     ])
 
-
-#Use pipeline to train the model
+# # Building Model by using Naïve Bayes
+from sklearn.naive_bayes import MultinomialNB
 text_clf.fit(X_train,y_train ) 
 
 #Test model accuracy
 import numpy as np
 from sklearn import metrics 
 predicted = text_clf.predict(X_test)
-#np.mean(predicted == y_test) 
-print(metrics.confusion_matrix(y_test, predicted))
-print(np.mean(predicted == y_test) )
 
+print('Result of Naïve Bayes:')
+print(metrics.confusion_matrix(y_test, predicted)) # Confusion Matrix
+print(np.mean(predicted == y_test)) # Correct Rate
 
-# # Building a Decision Tree Model
-#Decision Tree
+# # Building Model by using Decision Tree Model
 from sklearn import tree
 text_clf = Pipeline([('vect', CountVectorizer()),
                      ('tfidf', TfidfTransformer()),
@@ -125,12 +101,11 @@ text_clf = Pipeline([('vect', CountVectorizer()),
 text_clf.fit(X_train, y_train) 
 predicted = text_clf.predict(X_test)
 
+print('Result of Decision Tree:')
 print(metrics.confusion_matrix(y_test, predicted))
 print(np.mean(predicted == y_test) )
 
-
-# # Building a SVM Model
-#SVM
+# # Building Model by using SVM
 from sklearn.linear_model import SGDClassifier
 text_clf = Pipeline([('vect', CountVectorizer()),
                      ('tfidf', TfidfTransformer(use_idf=True)),
@@ -138,18 +113,18 @@ text_clf = Pipeline([('vect', CountVectorizer()),
                                             alpha=1e-3 
                                              ))
                     ])
-
 text_clf.fit(X_train, y_train)  
 
 predicted = text_clf.predict(X_test)
  
+print('Result of Support Vector Machine:')
 print(metrics.confusion_matrix(y_test, predicted))
 print(np.mean(predicted == y_test) )
-#y_test.value_counts()
-
 print(metrics.classification_report(y_test, predicted))
 
 
+
+## Confusion Matrix Output Optimize
 import itertools
 import matplotlib.pyplot as plt
 def plot_confusion_matrix(cm, classes,
@@ -211,10 +186,7 @@ parameters = {
 
 # If we give this parameter a value of -1, 
 #grid search will detect how many cores are installed and uses them all:
-
 gs_clf = GridSearchCV(text_clf, parameters, n_jobs=-1)
 gs_clf = gs_clf.fit(X_train, y_train)
 for param_name in sorted(parameters.keys()):
     print("%s: %r" % (param_name, gs_clf.best_params_[param_name]))
-
-
